5afe75451505cbdb20f80994e3c428aa0469bdea
TheSoniix <julianscheffler22@gmail.com>
2020-12-13 00:07:57 +0100

    Add html changes

--- src/components/AddFormular.vue
+++ src/components/AddFormular.vue
@@ -1,15 +1,18 @@
 <template>
-  <form v-on:submit.prevent="$emit('add-user', [firstname, secondname, description]); clear()">
-    <div class="form-group">
-      <label for="firstNameInput">Firstname</label>
-      <input type="text" class="form-control" id="firstNameInput" v-model="firstname">
-      <label for="secondNameInput">Secondname</label>
-      <input type="text" class="form-control" id="secondNameInput" v-model="secondname">
-      <span>Description</span>
-      <b-form-textarea type="text" class="form-control" id="descriptionInput" v-model="description"></b-form-textarea>
-    </div>
-    <button type="submit" class="btn btn-success">Add</button>
-  </form>
+  <div>
+    <h5>New User</h5>
+    <form v-on:submit.prevent="$emit('add-user', [firstname, secondname, description]); clear()">
+      <div class="form-group">
+        <label for="firstNameInput">Firstname</label>
+        <input type="text" class="form-control" id="firstNameInput" v-model="firstname">
+        <label for="secondNameInput">Secondname</label>
+        <input type="text" class="form-control" id="secondNameInput" v-model="secondname">
+        <span>Description</span>
+        <b-form-textarea type="text" class="form-control" id="descriptionInput" v-model="description"></b-form-textarea>
+      </div>
+      <button type="submit" class="btn btn-success">Add</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -17,20 +20,20 @@ import Vue from 'vue';
 
 
 export default Vue.extend({
-name: "AddFormular",
+  name: "AddFormular",
   data() {
-  return {
-    firstname: '',
-    secondname: '',
-    description: '',
-  }
+    return {
+      firstname: '',
+      secondname: '',
+      description: '',
+    }
   },
   methods: {
-  clear: function () {
-    this.firstname = '';
-    this.secondname = '';
-    this.description = '';
-  }
+    clear: function () {
+      this.firstname = '';
+      this.secondname = '';
+      this.description = '';
+    }
   }
 });
 </script>
